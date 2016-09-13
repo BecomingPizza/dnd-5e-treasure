b0c3ba5220eb0802a8a5e4a17b04e63b4a135bfe
BecomingPizza <ashleyroots@googlemail.com>
2016-09-13 17:40:35 +0100

    Possibly made a branch?

--- app/src/main/java/com/pizzatech/dnd_5e_treasure/MainActivity.java
+++ app/src/main/java/com/pizzatech/dnd_5e_treasure/MainActivity.java
@@ -186,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
             case 27:case 28:case 29:case 30:case 31:case 32:case 33:case 34:case 35:case 36:
                 break;
             case 37:case 38:case 39:case 40:case 41:case 42:case 43:case 44:
+                rollGems(2, 6, 10);
                 break;
             case 45:case 46:case 47:case 48:case 49:case 50:case 51:case 52:
                 break;
