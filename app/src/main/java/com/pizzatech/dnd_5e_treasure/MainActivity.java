33f811fb43afc256381e8ef5300622620eca6290
BecomingPizza <ashleyroots@googlemail.com>
2016-09-13 14:05:18 +0100

    Added the CR selection dropdown

--- app/src/main/java/com/pizzatech/dnd_5e_treasure/MainActivity.java
+++ app/src/main/java/com/pizzatech/dnd_5e_treasure/MainActivity.java
@@ -2,6 +2,8 @@ package com.pizzatech.dnd_5e_treasure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner cr_spinner = (Spinner) findViewById(R.id.cr_selection_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cr_selection_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cr_spinner.setAdapter(adapter);
     }
 }
