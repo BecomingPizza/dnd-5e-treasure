aed95fb22a86b75599344d41b0649fe95ea245c2
veetox <mattjl1985@gmail.com>
2016-09-23 10:37:33 +0100

    Completed coding rollMagicE().

--- app/src/main/java/com/pizzatech/dnd_5e_treasure/TreasureRoller.java
+++ app/src/main/java/com/pizzatech/dnd_5e_treasure/TreasureRoller.java
@@ -1667,9 +1667,9 @@ class TreasureRoller extends AsyncTask {
             case 11: case 12: case 13: case 14: case 15:
             case 16: case 17: case 18: case 19: case 20:
 
-                magicText = treasureArraySub[230];
-                subText = res.getString(R.string.dmg187);
-                break;
+            magicText = treasureArraySub[230];
+            subText = res.getString(R.string.dmg187);
+            break;
 
             case 21: case 22: case 23: case 24: case 25:
             case 26: case 27: case 28: case 29: case 30:
@@ -1777,17 +1777,64 @@ class TreasureRoller extends AsyncTask {
         String magicText = "";
         String subText = "";
 
-        //TODO: Populate page array in strings.xml and add references to switch statement
-        //TODO: Populate switch
-
         switch(roll)
         {
+            case 1: case 2: case 3: case 4: case 5:
+            case 6: case 7: case 8: case 9: case 10:
+            case 11: case 12: case 13: case 14: case 15:
+            case 16: case 17: case 18: case 19: case 20:
+            case 21: case 22: case 23: case 24: case 25:
+            case 26: case 27: case 28: case 29: case 30:
 
+            magicText = treasureArraySub[294];
+            subText = res.getString(R.string.dmg200);
+            break;
+
+            case 31: case 32: case 33: case 34: case 35:
+            case 36: case 37: case 38: case 39: case 40:
+            case 41: case 42: case 43: case 44: case 45:
+            case 46: case 47: case 48: case 49: case 50:
+            case 51: case 52: case 53: case 54: case 55:
+
+            magicText = treasureArraySub[228];
+            subText = res.getString(R.string.dmg187);
+            break;
+
+            case 56: case 57: case 58: case 59: case 60:
+            case 61: case 62: case 63: case 64: case 65:
+            case 66: case 67: case 68: case 69: case 70:
+
+            magicText = treasureArraySub[230];
+            subText = res.getString(R.string.dmg187);
+            break;
+
+            case 71: case 72: case 73: case 74: case 75:
+            case 76: case 77: case 78: case 79: case 80:
+            case 81: case 82: case 83: case 84: case 85:
+
+            magicText = treasureArraySub[295];
+            subText = res.getString(R.string.dmg200);
+            break;
+
+            case 86: case 87: case 88: case 89: case 90:
+            case 91: case 92: case 93:
+
+            magicText = treasureArraySub[329];
+            subText = res.getString(R.string.dmg209);
+            break;
+
+            case 99: case 100:
+
+            magicText = treasureArraySub[286];
+            subText = res.getString(R.string.dmg200);
+            break;
 
         }
 
         addToList(magicText, subText);
     }
+
+
     /**
      * Roll a D100 on table F to determine
      * a Magic Item
