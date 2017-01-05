1bdb9a002c57195af1308681b9b9f7f927e9d4bf
BecomingPizza <ashleyroots@googlemail.com>
2017-01-05 22:04:06 +0000

    possibly functional sql upgrade script?

--- app/src/main/java/com/pizzatech/dnd_5e_treasure/DBHelper.java
+++ app/src/main/java/com/pizzatech/dnd_5e_treasure/DBHelper.java
@@ -13,7 +13,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 class DBHelper extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "PT5EDB.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
