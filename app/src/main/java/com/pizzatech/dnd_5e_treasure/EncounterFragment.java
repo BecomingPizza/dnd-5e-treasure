e7c4e30e4705097fdfc940366a085342e85eed27
BecomingPizza <ashleyroots@googlemail.com>
2017-03-06 22:44:34 +0000

    fixed #45

--- app/src/main/java/com/pizzatech/dnd_5e_treasure/EncounterFragment.java
+++ app/src/main/java/com/pizzatech/dnd_5e_treasure/EncounterFragment.java
@@ -320,12 +320,12 @@ public class EncounterFragment extends Fragment {
         ArrayList<IndvLootResult> stuff = new ArrayList<>();
 
         EncounterEnemiesListItem e;
-        Integer cr;
+        double cr;
         Integer table = -1;
 
         for (int i = 0; i < encounterEnemiesList.size(); i++) {
             e = encounterEnemiesList.get(i);
-            cr = Integer.parseInt(e.getCr());
+            cr = tacticalParse(e.getCr());
             if (cr >= 0 && cr <= 4) {
                 table = 0;
             } else if (cr >= 5 && cr <= 10) {
